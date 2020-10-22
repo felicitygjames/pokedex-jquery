e8ab3076e56ffaaab5b1d1ea92d9633a31e40cdb
felicitygjames <72702947+felicitygjames@users.noreply.github.com>
2020-10-22 15:10:22 -0500

    created pokemonList array and added details

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,1 @@
-alert("Hello World");
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+let pokemonList = [{name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']}, {name: 'Ivysaur', height: 1, types: ['grass', 'poison']}, {name: 'Venusaur', height: 2, types: ['grass', 'poison']}, {name: 'Charmander', height: 0.6, types: 'fire'}];
