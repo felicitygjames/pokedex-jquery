4710ee63f4882032139cc23478bacde846b34a58
felicitygjames <72702947+felicitygjames@users.noreply.github.com>
2020-10-23 19:54:46 -0500

    added for loop for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", "poison"] },
-  { name: "Ivysaur", height: 1, types: ["grass", "poison"] },
-  { name: "Venusaur", height: 2, types: ["grass", "poison"] },
-  { name: "Charmander", height: 0.6, types: "fire" },
+  { name: "Bulbasaur (height: 0.7)", height: 0.7, types: ["grass", "poison"] },
+  { name: "Ivysaur (height: 1)", height: 1, types: ["grass", "poison"] },
+  { name: "Venusaur (height: 2)", height: 2, types: ["grass", "poison"] },
+  { name: "Charmander (height 0.6)", height: 0.6, types: "fire" },
 ];
+
+let text = "";
+let i = 0;
+for (;pokemonList[i];) {
+  text = text + " " + pokemonList[i].name; i++;
+}
+document.write(text);
