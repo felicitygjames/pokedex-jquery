27a4c27c0e7d481a4fef2fc7041acded9a8a6d5a
felicitygjames <72702947+felicitygjames@users.noreply.github.com>
2020-10-25 12:12:24 -0500

    added loop and styling to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,42 @@
 let pokemonList = [
-  { name: "Bulbasaur (height: 0.7)", height: 0.7, types: ["grass", "poison"] },
-  { name: "Ivysaur (height: 1)", height: 1, types: ["grass", "poison"] },
-  { name: "Venusaur (height: 2)", height: 2, types: ["grass", "poison"] },
-  { name: "Charmander (height 0.6)", height: 0.6, types: "fire" },
+  { name: "Bulbasaur", height: 0.7, types: ["grass", " poison"] },
+  { name: "Ivysaur", height: 1, types: ["water", " poison"] },
+  { name: "Venusaur", height: 2, types: ["flying", " poison"] },
+  { name: "Charmander", height: 0.6, types: ["fire"] },
 ];
 
-let text = "";
-let i = 0;
-for (;pokemonList[i];) {
-  text = text + " " + pokemonList[i].name; i++;
-}
-document.write(text);
-
-for (let i = 0; i < pokemonList.length; i++){
-  if (pokemonList[i].height > 1.5){
-    document.write(" - Wow, that's a big pokemon!");
-  } else if (pokemonList[i].height <= 1.5){
-    document.write();
+for (let i = 0; i < pokemonList.length; i++) {
+  let color = "";
+  for (let k = 0; k < pokemonList[i].types.length; k++){
+    if (pokemonList[i].types[k] === "grass"){
+      color = '<span style="color:green;">'
+    } else if (pokemonList[i].types[k] === "flying"){
+      color = '<span style="color:lightblue;">'
+    } else if (pokemonList[i].types[k] === "water") {
+      color = '<span style="color:blue;">'
+    } else if (pokemonList[i].types[k] === "fire") {
+      color = '<span style="color:red;">'
+    }
+  } 
+  let size = "";
+  if (pokemonList[i].height > 1){
+    size = "It's a big pokemon!";
+  } else if(pokemonList[i].height < 0.7){
+    size = "It's a small pokemon!";
+  } else {
+    size = "It's an average pokemon";
   }
+  document.write(
+    '<div class = "box">' +
+      pokemonList[i].name +
+      " (height: " +
+      pokemonList[i].height +
+      ") " +
+      size + 
+      color +
+      "<br>" +
+      pokemonList[i].types +
+      "<br>" +
+      "</div>"
+  );
 }
