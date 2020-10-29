e5a7905267dfaaacb8163543dea6c6085bdd1fef
felicitygjames <72702947+felicitygjames@users.noreply.github.com>
2020-10-28 21:34:03 -0500

    switched pokemonList from a for loop to a forEach function

--- js/scripts.js
+++ js/scripts.js
@@ -5,38 +5,42 @@ let pokemonList = [
   { name: "Charmander", height: 0.6, types: ["fire"] },
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-  let color = "";
-  for (let k = 0; k < pokemonList[i].types.length; k++){
-    if (pokemonList[i].types[k] === "grass"){
-      color = '<span style="color:green;">'
-    } else if (pokemonList[i].types[k] === "flying"){
-      color = '<span style="color:lightblue;">'
-    } else if (pokemonList[i].types[k] === "water") {
-      color = '<span style="color:blue;">'
-    } else if (pokemonList[i].types[k] === "fire") {
-      color = '<span style="color:red;">'
-    }
-  } 
-  let size = "";
-  if (pokemonList[i].height > 1){
-    size = "It's a big pokemon!";
-  } else if(pokemonList[i].height < 0.7){
-    size = "It's a small pokemon!";
-  } else {
-    size = "It's an average pokemon";
-  }
-  document.write(
-    '<div class = "box">' +
-      pokemonList[i].name +
-      " (height: " +
-      pokemonList[i].height +
-      ") " +
-      size + 
-      color +
-      "<br>" +
-      pokemonList[i].types +
-      "<br>" +
-      "</div>"
-  );
-}
+// for (let i = 0; i < pokemonList.length; i++) {
+//   let color = "";
+//   for (let k = 0; k < pokemonList[i].types.length; k++){
+//     if (pokemonList[i].types[k] === "grass"){
+//       color = '<span style="color:green;">'
+//     } else if (pokemonList[i].types[k] === "flying"){
+//       color = '<span style="color:lightblue;">'
+//     } else if (pokemonList[i].types[k] === "water") {
+//       color = '<span style="color:blue;">'
+//     } else if (pokemonList[i].types[k] === "fire") {
+//       color = '<span style="color:red;">'
+//     }
+//   } 
+//   let size = "";
+//   if (pokemonList[i].height > 1){
+//     size = "It's a big pokemon!";
+//   } else if(pokemonList[i].height < 0.7){
+//     size = "It's a small pokemon!";
+//   } else {
+//     size = "It's an average pokemon";
+//   }
+//   document.write(
+//     '<div class = "box">' +
+//       pokemonList[i].name +
+//       " (height: " +
+//       pokemonList[i].height +
+//       ") " +
+//       size + 
+//       color +
+//       "<br>" +
+//       pokemonList[i].types +
+//       "<br>" +
+//       "</div>"
+//   );
+// }
+
+pokemonList.forEach(function(pokemon) {
+  document.write(pokemon.name + " (" + pokemon.height + ") " + "<br>");
+});
