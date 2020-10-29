d34956a95b66ad980e0967376c2cf77721bc7675
felicitygjames <72702947+felicitygjames@users.noreply.github.com>
2020-10-29 15:52:59 -0500

    fixed forEach and IIFE for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,71 @@
-let pokemonList = [
-  { name: "Bulbasaur", height: 0.7, types: ["grass", " poison"] },
-  { name: "Ivysaur", height: 1, types: ["water", " poison"] },
-  { name: "Venusaur", height: 2, types: ["flying", " poison"] },
-  { name: "Charmander", height: 0.6, types: ["fire"] },
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, types: ["grass", " poison"] },
+    { name: "Ivysaur", height: 1, types: ["water", " poison"] },
+    { name: "Venusaur", height: 2, types: ["flying", " poison"] },
+    { name: "Charmander", height: 0.6, types: ["fire"] },
+  ];
+  function add(pokemon) {
+    if (
+      typeof pokemon === "object" &&
+      "name" in pokemon &&
+      "height" in pokemon &&
+      "types" in pokemon
+    ) {
+      pokemonList.push(pokemon);
+    } else {
+      alert("Please enter a valid pokemon");
+    }
+  }
+  function getAll() {
+    return pokemonList;
+  }
+  return {
+    getAll: getAll,
+    add: add,
+  };
+})();
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({
+  name: "Pikachu",
+  height: 0.9,
+  types: ["electric", " poison"],
+});
 
-// for (let i = 0; i < pokemonList.length; i++) {
-//   let color = "";
-//   for (let k = 0; k < pokemonList[i].types.length; k++){
-//     if (pokemonList[i].types[k] === "grass"){
-//       color = '<span style="color:green;">'
-//     } else if (pokemonList[i].types[k] === "flying"){
-//       color = '<span style="color:lightblue;">'
-//     } else if (pokemonList[i].types[k] === "water") {
-//       color = '<span style="color:blue;">'
-//     } else if (pokemonList[i].types[k] === "fire") {
-//       color = '<span style="color:red;">'
-//     }
-//   } 
-//   let size = "";
-//   if (pokemonList[i].height > 1){
-//     size = "It's a big pokemon!";
-//   } else if(pokemonList[i].height < 0.7){
-//     size = "It's a small pokemon!";
-//   } else {
-//     size = "It's an average pokemon";
-//   }
-//   document.write(
-//     '<div class = "box">' +
-//       pokemonList[i].name +
-//       " (height: " +
-//       pokemonList[i].height +
-//       ") " +
-//       size + 
-//       color +
-//       "<br>" +
-//       pokemonList[i].types +
-//       "<br>" +
-//       "</div>"
-//   );
-// }
-
-pokemonList.forEach(function(pokemon) {
-  document.write(pokemon.name + " (" + pokemon.height + ") " + "<br>");
+console.log(pokemonRepository.getAll());
+pokemonRepository.getAll().forEach(function (pokemon) {
+  let color = "";
+  if (pokemon.types.includes("grass")) {
+    color = '<span style="color:green;">';
+  } else if (pokemon.types.includes("flying")) {
+    color = '<span style="color:lightblue;">';
+  } else if (pokemon.types.includes("water")) {
+    color = '<span style="color:blue;">';
+  } else if (pokemon.types.includes("fire")) {
+    color = '<span style="color:red;">';
+  } else if (pokemon.types.includes("electric")) {
+    color = '<span style="color:yellow;">';
+  }
+  
+  let size = "";
+  if (pokemon.height > 1) {
+    size = "It's a big pokemon!";
+  } else if (pokemon.height < 0.7) {
+    size = "It's a small pokemon!";
+  } else {
+    size = "It's an average pokemon";
+  }
+  document.write(
+    '<div class = "box">' +
+      pokemon.name +
+      " (height: " +
+      pokemon.height +
+      ") " +
+      size +
+      color +
+      "<br>" +
+      pokemon.types +
+      "<br>" +
+      "</div>"
+  );
 });
