8ca5bb58ba394805d8ae89f67c9bec82d7344ad9
felicitygjames <72702947+felicitygjames@users.noreply.github.com>
2020-11-14 20:06:30 -0600

    fixed minified js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,7 @@ let pokemonRepository = (function () {
   let pokemonList = [];
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
   function add(pokemon) {
-    if (
-      typeof pokemon === "object" &&
-      "name" in pokemon
-      // &&
-      // "detailsUrl" in pokemon
-    ) {
+    if (typeof pokemon === "object" && "name" in pokemon) {
       pokemonList.push(pokemon);
     } else {
       alert("Please enter a valid pokemon");
